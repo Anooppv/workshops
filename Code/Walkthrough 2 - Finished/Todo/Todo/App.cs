5a6530ccd1587add91319086477301bfd9295512
Pierce Boggan <pierce@xamarin.com>
2015-09-30 13:28:28 -0400

    Updated Walkthrough #2 - Finished

--- Code/Walkthrough 2 - Finished/Todo/Todo/App.cs
+++ Code/Walkthrough 2 - Finished/Todo/Todo/App.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 
 using Xamarin.Forms;
-using Microsoft.WindowsAzure.MobileServices;
 
 namespace Todo
 {
@@ -12,28 +11,12 @@ namespace Todo
     {
         public App()
         {
-			SetupMobileService ();
-
             // The root page of your application
 			MainPage = new NavigationPage (new TodoPage ()) {
 				BarTextColor = Color.White,
 				BarBackgroundColor = Color.FromHex ("2C97DE")
 			};
         }
-
-		private async void SetupMobileService ()
-		{        
-			var applicationURL = @"https://tododemoapplication.azure-mobile.net/";
-			var applicationKey = @"xRelREGxnCOaYeXKUpqxFDRZKADbaV37";
-
-			MobileService.Client = new MobileServiceClient (applicationURL, applicationKey);
-			var items = await MobileService.Client.GetTable <TodoItem> ().ToListAsync ();
-
-			if (items == null)
-				System.Diagnostics.Debug.WriteLine ("NULL");
-			else
-				System.Diagnostics.Debug.WriteLine (items.Count);	
-		}
 
         protected override void OnStart()
         {
