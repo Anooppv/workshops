5c7fb352cf915906e3e1e06eda6bf3472ebba7ab
Pierce Boggan <pierce@xamarin.com>
2015-09-29 21:44:30 -0400

    Added Walkthrough #1

---
+++ Code/Walkthrough 1 - Finished/Todo/Todo/View Models/TodoViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace Todo
+{
+	public class TodoViewModel : BaseViewModel
+	{
+		public ObservableCollection<TodoItem> Todos { get; set; }
+
+		public TodoViewModel ()
+		{
+			Todos = new ObservableCollection<TodoItem> ();
+			Todos.Add (new TodoItem { Name = "Reading assignment", Description = "Read chapters 29-34 and take notes." });
+			Todos.Add (new TodoItem { Name = "Math homework", Description = "Complete problems 1-14 on worksheet." });
+			Todos.Add (new TodoItem { Name = "Todo app", Description = "Build a todo app for my CS class" });
+		}
+	}
+}
---
+++ Code/Walkthrough 1b - Start/Todo/Todo/App.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Todo
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+			MainPage = new ContentPage {
+				Content =  new Label
+				{
+					Text = "Hello, Forms !",
+					VerticalOptions = LayoutOptions.CenterAndExpand,
+					HorizontalOptions = LayoutOptions.CenterAndExpand,
+				}
+			};
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
